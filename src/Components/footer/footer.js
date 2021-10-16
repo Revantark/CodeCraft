5f96cc4d78e49e43ee4dfe804d7f16ca98de7dea
RevantArk <revantark@gmail.com>
2021-10-16 18:48:09 +0530

    Update UI

--- src/Components/footer/footer.js
+++ src/Components/footer/footer.js
@@ -1,9 +1,7 @@
 import './footer.css'
 import React from 'react'
-import {useHistory} from 'react-router-dom'
 
 export default function Footer(){
-    const history= useHistory();
     
     return(
         <section className="footer">
@@ -19,7 +17,7 @@ export default function Footer(){
                     <div className="border1"></div>
                     <ul className="list">
                         <a href="#landingPage" ><li>Home</li></a>
-                        <a  onClick={()=>history.replace('/register')}>Register</a>
+                        <a  href='register'>Register</a>
                         <a href="#codeCraft" ><li>About us</li></a>
                     </ul>
                 </div>
@@ -27,13 +25,13 @@ export default function Footer(){
                     <h3>Contact Us</h3>
                     <div className="border1"></div>
                     <ul className="list">
-                        <a href="https://g.page/coding-studio?share" target="_blank"> 
+                        <a href="https://g.page/coding-studio?share" target="_blank" rel="noreferrer"> 
                         <li><i className="fa fa-map-marker"></i>c.S(); HQ-VBIT,Hyd</li>
                         </a>
-                        <a href="tel:+918790805406" target="_blank"> 
+                        <a href="tel:+918790805406" target="_blank" rel="noreferrer"> 
                         <li><i className="fa fa-phone"></i>+918790805406</li>
                         </a>
-                        <a href="mailto:executives@codingstudio.club" target="_blank">
+                        <a href="mailto:executives@codingstudio.club" target="_blank" rel="noreferrer">
                         <li><i className="fa fa-envelope"></i>executives@codingstudio.club</li>
                         </a>
                     </ul>
@@ -42,10 +40,10 @@ export default function Footer(){
                     <h3>Follow Us on</h3>
                     <div className="border1"></div>
                     <ul className="list">
-                        <a href="https://www.instagram.com/coding.studio/" target="_blank"> 
+                        <a href="https://www.instagram.com/coding.studio/" target="_blank" rel="noreferrer"> 
                         <li><i className="fab fa-instagram"></i><a href="/home">Instagram</a></li>
                         </a>
-                        <a href="https://discord.gg/8CwuPh3Nbv" target="_blank"> 
+                        <a href="https://discord.gg/8CwuPh3Nbv" target="_blank" rel="noreferrer"> 
                         <li><i class="fab fa-discord"></i><a href="/home">Discord</a></li>
                         </a>
                     </ul>
